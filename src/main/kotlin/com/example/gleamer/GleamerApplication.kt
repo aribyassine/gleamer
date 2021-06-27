92bfaa96044300a747d6b866129e83276d98531e
Your Name <you@example.com>
2021-06-27 11:58:04 +0200

    init

---
+++ src/main/kotlin/com/example/gleamer/GleamerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.gleamer
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GleamerApplication
+
+fun main(args: Array<String>) {
+	runApplication<GleamerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/gleamer/GleamerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.gleamer
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GleamerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
